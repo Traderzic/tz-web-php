4960afb0586b162d1a57be1b229bbb461db990b5
Hastur <lukas.brasseleur@gmail.com>
2016-06-25 12:59:01 +0200

    Update EventTest.php

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -1,10 +1,11 @@
 <?php 
-
 require_once('../models/ProjectClass.php');
 require_once('../models/ProjectDAO.php');
+require_once('../models/Event.php');
+require_once('../models/EventDAO.php');
 use PHPUnit\Framework\TestCase;
 
-class ProjectClassTest extends PHPUnit_Framework_TestCase	{
+class EventTest extends PHPUnit_Framework_TestCase	{
 	
 	public $name = "simon";
 	public $startDate = '2016-12-12';
@@ -12,9 +13,9 @@ class ProjectClassTest extends PHPUnit_Framework_TestCase	{
 	public $endDate = '2015-12-12';
 	public $rate = 4;
 	public $status = 'conception';
-  public $classe = 'event';
-  public $location = 'lieusaint';
-  public $day = '2017-12-12';
+  	public $classe = 'event';
+  	public $location = 'lieusaint';
+  	public $day = '2017-12-12';
 
 	
 	public function testSetLocation()	{
