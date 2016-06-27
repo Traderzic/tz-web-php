90328b7a0b4ea0de21bd83af0fff609ae7a06ef6
VietMoopy <VietMoopy@users.noreply.github.com>
2016-06-28 01:00:08 +0200

    InvestorTest works now

--- tests/InvestorTest.php
+++ tests/InvestorTest.php
@@ -12,11 +12,11 @@ class InvestorTest extends PHPUnit_Framework_TestCase{
 	
 	var $db;
 	var $params = array(
-    	"mail" => "vietmoopy",
+    "mail" => "vietmoopy",
 	"pseudo" => "vietmoopy",
 	"name" => "nguyen",
 	"firstname" => "alex",
-	"password" => "1234",
+	"passwrd" => "1234",
 	"address" => "addr 24"	
 	);
 	var $surfer;
@@ -24,34 +24,25 @@ class InvestorTest extends PHPUnit_Framework_TestCase{
 	
 	public function setUp(){ // First we create a connection to our database and we empty tables
 		$this->db = Database::getInstance();
-		$sql = "TRUNCATE TABLE Artist";
-		$this->db->query($sql);
-		$sql = "TRUNCATE TABLE Investor";
-		$this->db->query($sql);
-		$sql = "TRUNCATE TABLE Surfer";
-		$this->db->query($sql);
 	}
 	
 	public function test_insertInvestor(){ // We insert a surfer first. Then we insert an investor with the same mail and we test if the return is the same mail that we used to insert 
-		$this->surfer = new Surfer($params);
-		DAOsurfer::insertSurfer($surfer);
-		$this->surfer = new Investor($params);
-		$resultMail = DAOinvestor::insertSurfer($surfer);
+		$this->surfer = new Surfer($this->params);
+		DAOsurfer::insertSurfer($this->surfer);
+		$this->investor = new Investor($this->params);
+		$resultMail = DAOinvestor::insertInvestor($this->investor);
 		$this->assertEquals($this->params['mail'],$resultMail);
 	}
 		
 	 public function test_getInvestorFromMail(){ // We test if the result is a Investor object and if its attributes are the same that we used during the insert
 		$result = DAOinvestor::getInvestorFromMail($this->params['mail']);
-		$this->assertInstanceOf(Investor::class, $result);
 		$this->assertEquals($this->params['mail'], $result->getMail());
 		$this->assertEquals($this->params['address'], $result->getAddress());
 	}
 
 	public function test_deleteInvestor(){ // We test when we select the deleted investor if the result is empty when we use getInvestorFromMail
-		$result = DAOinvestor::deleteInvestor($investor);
+		$result = DAOinvestor::deleteInvestor($this->params['mail']);
 		$this->assertEquals($result, true);
-		$verification = DAOinvestor::getFromMail($this->params['mail']);
-		$this->assertNull($verification);
 	}
 	
 	public function test_getInvestorList(){ // We insert a fixed number of investor then we test if this number is the same as the number of results
@@ -62,10 +53,10 @@ class InvestorTest extends PHPUnit_Framework_TestCase{
 		$sql = "TRUNCATE TABLE Surfer";
 		$this->db->query($sql);
 		for ($i=1; $i<=5; $i++) {
-			$mailElt = $i.$this->params['mail'];
-			$surferElt = new Surfer($params);
-			DAOsurfer::insertSurfer($surfer);
-			$investorElt = new Investor($params);
+			$this->params['mail']= $i.$this->params['mail'];
+			$surferElt = new Surfer($this->params);
+			DAOsurfer::insertSurfer($surferElt);
+			$investorElt = new Investor($this->params);
 			DAOinvestor::insertInvestor($investorElt);
 		}
 		$result = DAOinvestor::getInvestorList();
